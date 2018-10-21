80c2146967d27d1f059117f0817bea0e7f8d0790
Adam <adam@example.com>
2018-10-21 22:45:09 +0200

    Add app template

---
+++ app/src/main/java/com/ita/adamtkaczyk/provapp/MainScreenActivity.java
@@ -0,0 +1,15 @@
+package com.ita.adamtkaczyk.provapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ita.adamtkaczyk.provapp.R;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
